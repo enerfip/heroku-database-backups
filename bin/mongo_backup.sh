62176df99567a37b5cf1697e6e3954fd0a058e52
arnaud sellenet <arnodmental@gmail.com>
2019-07-16 18:41:32 +0200

    Add script to backup mongo databases

---
+++ bin/mongo_backup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# terminate script as soon as any command fails
+set -e
+
+if [[ -z "$APP" ]]; then
+  echo "Missing APP variable which must be set to the name of your app where the db is located"
+  exit 1
+fi
+
+if [[ -z "$S3_BUCKET_PATH" ]]; then
+  echo "Missing S3_BUCKET_PATH variable which must be set the directory in s3 where you would like to store your database backups"
+  exit 1
+fi
+
+#install aws-cli
+curl https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -o awscli-bundle.zip
+unzip awscli-bundle.zip
+chmod +x ./awscli-bundle/install
+./awscli-bundle/install -i /tmp/aws
+rm -Rf ./awscli-bundle
+rm awscli-bundle.zip
+
+MONGO_URI=$(heroku config:get MONGODB_URI -a $APP)
+
+BACKUP_FILE_NAME="$(date +"%Y-%m-%d-%H-%M")-$APP.mongodump"
+
+/usr/bin/mongodump --uri $MONGO_URI -o $BACKUP_FILE_NAME
+
+heroku pg:backups capture $DATABASE --app $APP
+curl -o $BACKUP_FILE_NAME `heroku pg:backups:url --app $APP`
+FINAL_FILE_NAME=$BACKUP_FILE_NAME
+
+if [[ -z "$NOGZIP" ]]; then
+  gzip $BACKUP_FILE_NAME
+  rm $BACKUP_FILE_NAME
+  FINAL_FILE_NAME=$BACKUP_FILE_NAME.gz
+fi
+
+if [[ "$GPG_PASSPHRASE" ]]; then
+  gpg -c --batch --passphrase $GPG_PASSPHRASE $FINAL_FILE_NAME
+  rm $FINAL_FILE_NAME
+  FINAL_FILE_NAME=$FINAL_FILE_NAME.gpg
+fi
+
+/tmp/aws/bin/aws s3 cp $FINAL_FILE_NAME s3://$S3_BUCKET_PATH/$APP/$FINAL_FILE_NAME
+rm $FINAL_FILE_NAME
+
+echo "backup $FINAL_FILE_NAME complete"
