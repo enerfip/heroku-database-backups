39b37dfb64ed61b5c89c1cb1c9f536a543844e4a
arnaud sellenet <arnaud@enerfip.fr>
2022-01-26 16:14:39 +0100

    Allow to customize database url and backup filename

--- bin/check_requirements.sh
+++ bin/check_requirements.sh
@@ -13,6 +13,11 @@ if [[ -z "$DATABASE" ]]; then
   echo "Using default DATABASE_URL. If you want to backup a specific database, please provide DATABASE=xxxx while invoking this command"
 fi
 
+if [[ -z "$FILENAME_PREFIX" ]]; then
+  FILENAME_PREFIX="$(date +"%Y-%m-%d-%H-%M")"
+  echo "Using default filename prefix: $FILENAME_PREFIX. If you want to override this value provide FILENAME_PREFIX while invoking this command"
+fi
+
 if [[ -z "$S3_BUCKET_PATH" ]]; then
   echo "Missing S3_BUCKET_PATH variable which must be set the directory in s3 where you would like to store your database backups"
   exit 1
