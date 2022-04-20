50391552bd3b8f45163a1b52552b286edc97de5f
arnaud sellenet <arnaud@enerfip.fr>
2022-04-20 08:57:07 +0200

    Add logging info

--- bin/backup.sh
+++ bin/backup.sh
@@ -10,6 +10,8 @@ MOD=$(expr $HOUR % 6)
 if [ $MOD != 0 ]; then
   echo "Scheduled only every 6 hours"
   exit 0
+else
+  echo "It's backup time!"
 fi
 
 if [ $KEEP_ONE_BACKUP_PER_WEEK != false ]; then
@@ -20,7 +22,11 @@ else
   BACKUP_FILE_NAME="$(date +"%Y-%m-%d-%H-%M")-$APP-$DATABASE.dump"
 fi
 
+echo "*** capturing $DATABASE on $APP and store it to $BACKUP_FILE_NAME ***"
+
 heroku pg:backups capture $DATABASE --app $APP
 curl -o $BACKUP_FILE_NAME `heroku pg:backups:url --app $APP`
+
+echo "*** capture done. Now uploading to S3 ***"
 
 BACKUP_FILE_NAME=$BACKUP_FILE_NAME ./bin/upload_backup_file.sh
