e8c5e3bc592f26901db2a01ed603c11c037e103a
arnaud sellenet <arnaud@enerfip.fr>
2022-05-04 09:14:15 +0200

    Add addition FREQUENCY_HOUR parameter

--- bin/backup.sh
+++ bin/backup.sh
@@ -5,22 +5,19 @@
 
 source ./bin/check_requirements.sh
 
-HOUR=$(date +"%-H")
-MOD=$(expr $HOUR % 6)
-
 if [[ -z "$KEEP_ONE_BACKUP_PER_WEEK" ]]; then
-  echo "- KEEP_ONE_BACKUP_PER_WEEK Yes"
+  echo "- KEEP_ONE_BACKUP_PER_WEEK No"
   KEEP_ONE_BACKUP_PER_WEEK=false
 else
-  echo "- KEEP_ONE_BACKUP_PER_WEEK No"
+  echo "- KEEP_ONE_BACKUP_PER_WEEK Yes"
   KEEP_ONE_BACKUP_PER_WEEK=true
 fi
 
 if [[ -z "$KEEP_ONE_BACKUP_PER_DAY" ]]; then
-  echo "- KEEP_ONE_BACKUP_PER_DAY Yes"
+  echo "- KEEP_ONE_BACKUP_PER_DAY No"
   KEEP_ONE_BACKUP_PER_DAY=false
 else
-  echo "- KEEP_ONE_BACKUP_PER_DAY No"
+  echo "- KEEP_ONE_BACKUP_PER_DAY Yes"
   KEEP_ONE_BACKUP_PER_DAY=true
 fi
 
@@ -32,13 +29,22 @@ else
   BACKUP_FILE_NAME="$(date +"%Y-%m-%d-%H-%M")-$APP-$DATABASE.dump"
 fi
 
+if [[ -z "$FREQUENCY_HOUR" ]]; then
+  FREQUENCY_HOUR=24
+fi
+
+echo "- FREQUENCY_HOUR $FREQUENCY_HOUR"
+
+HOUR=$(date +"%-H")
+MOD=$(expr $HOUR % $FREQUENCY_HOUR)
+
 echo "HOUR is set to $HOUR"
 echo "MOD is set to $MOD"
 
 if [[ $MOD -gt 0 ]]
 then
-  echo "Scheduled only every 6 hours"
-  echo "$MOD is not a multiple of 6. Current hour is $HOUR"
+  echo "Scheduled only every $FREQUENCY_HOUR hours"
+  echo "$MOD is not a multiple of $FREQUENCY_HOUR. Current hour is $HOUR"
 else
   echo "Backup time"
 
