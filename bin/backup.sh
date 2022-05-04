608be14369f3c1da8ea6b651c8ffb6e434024aee
arnaud sellenet <arnaud@enerfip.fr>
2022-05-04 08:58:27 +0200

    Refactor, avoid exit 0

--- bin/backup.sh
+++ bin/backup.sh
@@ -8,15 +8,6 @@ source ./bin/check_requirements.sh
 HOUR=$(date +"%H")
 MOD=$(expr $HOUR % 6)
 
-if [[ $MOD -gt 0 ]]
-then
-  echo "Scheduled only every 6 hours"
-  echo "$MOD is not a multiple of 6. Current hour is $HOUR"
-  exit 0
-else
-  echo "Backup time"
-fi
-
 if [[ -z "$KEEP_ONE_BACKUP_PER_WEEK" ]]; then
   echo "- KEEP_ONE_BACKUP_PER_WEEK Yes"
   KEEP_ONE_BACKUP_PER_WEEK=false
@@ -41,13 +32,15 @@ else
   BACKUP_FILE_NAME="$(date +"%Y-%m-%d-%H-%M")-$APP-$DATABASE.dump"
 fi
 
-echo "*** capturing $DATABASE on $APP and store it to $BACKUP_FILE_NAME ***"
+echo "HOUR is set to $HOUR"
+echo "MOD is set to $MOD"
 
-heroku pg:backups capture $DATABASE --app $APP
-LATEST_BACKUP=$(heroku pg:backups --app $APP | grep $DATABASE | head -n 1 | awk '{ print $1 }')
-BACKUP_URL=$(heroku pg:backups:url $LATEST_BACKUP --app $APP)
-curl -o $BACKUP_FILE_NAME $BACKUP_URL
+if [[ $MOD -gt 0 ]]
+then
+  echo "Scheduled only every 6 hours"
+  echo "$MOD is not a multiple of 6. Current hour is $HOUR"
+else
+  echo "Backup time"
 
-echo "*** capture done. Now uploading to S3 ***"
-
-BACKUP_FILE_NAME=$BACKUP_FILE_NAME ./bin/upload_backup_file.sh
+  BACKUP_FILE_NAME=$BACKUP_FILE_NAME DATABASE=$DATABASE APP=$APP ./bin/do_backup.sh
+fi
